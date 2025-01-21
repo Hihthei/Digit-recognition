bc8544422848e5d04030549e3edb425fe266eefa
Bouillon Célin <cbouillon@et.esiea.fr>
2025-01-21 11:57:45 +0100

    Refacto/Class_AI (#1)

--- source/AI/digitRecognition.py
+++ source/AI/digitRecognition.py
@@ -5,171 +5,208 @@ import matplotlib.pyplot as plt # visualation of the digit
 import tensorflow as tf # machine learning
 
 
-mnist = tf.keras.datasets.mnist
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+class DigitRecognition:
+    def __init__(self, model_name: str):
+        self.__models_dir = Path('AI/models')
+        self.__model_name = ""
+        self.__model = None
+        self.__history = None
 
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+        self.__x_train, self.__x_test, self.__y_train, self.__y_test = self.__load_and_preprocess_data()
 
-models_dir = Path('AI/models')
-
-
-def _model_name(model_path: Path) -> str:
-    if model_path is None:
-        model_path = Path("digitRecognition.keras")
-
-    models_name = models_dir / model_path
-    i = 1
-
-    while Path(models_name).exists():
-        models_name = models_dir / f"{model_path.stem}_{i}.keras"
-        i += 1
-
-    return str(models_name)
+        if self.__check_model(model_name):
+            self.__model_name = model_name
+            self.__load_model()
 
 
-def train_model(train_session: int, model_path: Path):
-    global x_train, x_test
-    x_train = np.expand_dims(x_train, axis=-1)
-    x_test = np.expand_dims(x_test, axis=-1)
+    def __load_and_preprocess_data(self):
+        mnist = tf.keras.datasets.mnist
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+        x_train = tf.keras.utils.normalize(x_train, axis=1)
+        x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+        x_train = np.expand_dims(x_train, axis=-1)
+        x_test = np.expand_dims(x_test, axis=-1)
+
+        return x_train, x_test, y_train, y_test
 
 
-    model = tf.keras.models.Sequential([
-        tf.keras.layers.Input(shape=(28, 28, 1)),
-
-        tf.keras.layers.Conv2D(16, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dense(63, activation='relu'),
-        tf.keras.layers.Dense(10, activation='softmax')
-    ])
-
-    early_stopping = tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss',
-        patience=3,
-        restore_best_weights=True
-    )
-
-    model.compile(
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0005),
-        loss='sparse_categorical_crossentropy',
-        metrics=['accuracy']
-    )
-    model.fit(
-        x_train, y_train,
-        epochs=train_session,
-        validation_split=0.2,
-        callbacks=[early_stopping]
-    )
-
-    model.save(_model_name(model_path))
+    def __get_model_path(self, model_name: str) -> Path:
+        __model__path = self.__models_dir / model_name
+        return __model__path
 
 
-def _load_model(model_path: Path):
-    if not model_path:
-        model_path = Path("digitRecognition.keras")
+    def train_model(self, train_session: int):
+        x_train = np.expand_dims(self.__x_train, axis=-1)
+        y_train = self.__y_train
 
-    models_name = models_dir / model_path
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Input(shape=(28, 28, 1)),
 
-    try:
-        print(f"Loading model: {models_name}")
-        return tf.keras.models.load_model(models_name)
-    except Exception as e:
-        print("No valid model")
-        print("Error:", e)
-        return None
+            tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
+            tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
-def evaluate_model(model_path: Path):
-    model = _load_model(model_path)
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(128, activation='relu'),
+            tf.keras.layers.Dense(10, activation='softmax')
+        ])
 
-    if model is None:
-        print("Model is None.")
-        return -1, -1
+        early_stopping = tf.keras.callbacks.EarlyStopping(
+            monitor='val_loss',
+            patience=3,
+            restore_best_weights=True
+        )
 
-    loss, accuracy = model.evaluate(x_test, y_test)
+        model.compile(
+            optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy']
+        )
+        __history = model.fit(
+            x_train, y_train,
+            epochs=train_session,
+            validation_split=0.2,
+            callbacks=[early_stopping]
+        )
 
-    pourcent_loss = round(loss * 100, 2)
-    pourcent_accuracy = round(accuracy * 100, 2)
+        model.save(self.__get_model_path(self.__model_name))
+        self.__model = model
+        self.__history = __history.history
 
-    return pourcent_loss, pourcent_accuracy
-
-
-def _display(img):
-    plt.imshow(img[0], cmap=plt.cm.binary)
-    plt.show()
-
-
-def _image_normalize(image_path):
-    img = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
-    img = cv2.resize(img, (28, 28))
-    img = np.invert(img)
-    img = img / 255.0
-    img = img.reshape(1, 28, 28)
-    return img
-
-'''    img = cv2.imread(str(image_path))[:,:,0]
-    # img = cv2.resize(img, (28, 28))
-    img = np.invert(np.array([img]))
-    # img_for_display = img
-    img = img.reshape(1, 28, 28)
-    return img # img_for_model, img_for_display'''
+        self.__display_training_history()
 
 
-def predict(image_path: Path, model_path: Path, model = None):
-    if model is None:
-        model = _load_model(model_path)
-        if model is None:
-            print("Model is None.")
-            return None
+    def __check_model(self, model_name: str):
+        __model_path = self.__get_model_path(model_name)
 
-    if not image_path.exists() or not image_path.suffix == '.png':
-        print("Image is invalid.")
-        return None
-
-    try:
-        # img_for_model, img = _image_normalize(image_path)
-        img = _image_normalize(image_path)
-        prediction = model.predict(img)
-        confidence = prediction.max()
-    except Exception as e:
-        print("Error:", e)
-        return None
-
-    else:
-        _display(img)
-        return np.argmax(prediction), confidence
+        if __model_path.exists():
+            return True
+        else:
+            self.__model_name = "DigitRecognition.keras"
+            return False
 
 
-def test_model(model_path: Path):
-    model = _load_model(model_path)
-
-    image_dir = Path("AI/test_data")
-
-    images = [f for f in image_dir.iterdir() if f.is_file() and f.suffix == '.png']
-
-    results = []
-
-    for image in images:
-        result = -1
+    def __load_model(self):
         try:
-            result, confidence = predict(image, model_path, model)
+            variable = self.__get_model_path(self.__model_name)
+            self.__model = tf.keras.models.load_model(variable)
 
-            if result is None:
-                print(f"Fail for image: {image}")
-            else:
-                print(f"This is probably a {result}!\n"
-                      f"I'm sure at {confidence*100:.2f}%")
+            print(self.__model)
 
         except Exception as e:
-            print("Error:", e)
-        finally:
-            answer = int(input("Correct answer: "))
-            results.append((image.stem, result, confidence, answer))
+            self.__model = None
+            raise e
 
-    return results
+
+    def evaluate_model(self):
+        if self.__model is None:
+            raise ValueError("Can't evaluate model, model is not loaded")
+
+        loss, accuracy = self.__model.evaluate(self.__x_test, self.__y_test)
+
+        pourcent_loss = round(loss * 100, 2)
+        pourcent_accuracy = round(accuracy * 100, 2)
+
+        return pourcent_loss, pourcent_accuracy
+
+
+    def __display(self, img):
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+
+
+    def __display_training_history(self):
+        plt.figure(figsize=(8, 6))
+
+        # Perte
+        plt.subplot(2, 1, 1)
+        plt.plot(self.__history['loss'], label='Loss', marker='o')
+        plt.plot(self.__history['val_loss'], label='Validation Loss', marker='x')
+        plt.title('Loss Over Epochs')
+        plt.xlabel('Epochs')
+        plt.ylabel('Loss')
+        plt.legend()
+        plt.grid(True)
+
+        # Précision
+        plt.subplot(2, 1, 2)
+        plt.plot(self.__history['accuracy'], label='Accuracy', marker='o')
+        plt.plot(self.__history['val_accuracy'], label='Validation Accuracy', marker='x')
+        plt.title('Accuracy Over Epochs')
+        plt.xlabel('Epochs')
+        plt.ylabel('Accuracy')
+        plt.legend()
+        plt.grid(True)
+
+        plt.tight_layout()
+        plt.show()
+
+
+    def __image_normalize(self, image_path):
+        img = cv2.imread(str(image_path), cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (28, 28))
+        img = np.invert(np.array(img))
+        img = img / 255.0
+        img = img.reshape(1, 28, 28)
+
+        return img
+
+
+    def predict(self, image_path: Path):
+        if self.__model is None:
+            raise ValueError("Can't evaluate model, model is not loaded")
+
+        if not image_path.exists() or not image_path.suffix == '.png':
+            raise ValueError("Image path is not valid")
+
+        try:
+            # img_for_model, img = _image_normalize(image_path)
+            img = self.__image_normalize(image_path)
+            prediction = self.__model.predict(img)
+            confidence = prediction.max()
+        except Exception as e:
+            raise e
+
+        else:
+            self.__display(img)
+            return np.argmax(prediction), confidence
+
+
+    def test_model(self):
+        if self.__model is None:
+            raise ValueError("Can't evaluate model, model is not loaded")
+
+        image_dir = Path("AI/test_data")
+
+        images = [f for f in image_dir.iterdir() if f.is_file() and f.suffix == '.png']
+
+        results = []
+        count = 0
+
+        for image in images:
+            try:
+                result, confidence = self.predict(image)
+
+                if result is None:
+                    print(f"Fail for image: {image}")
+                else:
+                    print(f"This is probably a {result}!\n"
+                          f"I'm sure at {confidence * 100:.2f}%")
+
+            except Exception as e:
+                raise e
+            else:
+                answer = int(input("Correct answer: "))
+                if result == answer:
+                    count += 1
+
+                results.append((image.stem, result, confidence, answer))
+
+        accuracy = count/len(results)
+
+        return results, accuracy
