3c46e5e49c28d09bd325ab119326383bf9f86024
hihthei <cbouillon@et.esiea.fr>
2025-01-21 11:51:25 +0100

    Refacto/Class_AI

--- source/main.py
+++ source/main.py
@@ -1,42 +1,24 @@
-from AI.digitRecognition import train_model, evaluate_model, test_model, predict
+from AI.digitRecognition import DigitRecognition
 
 from pathlib import Path
+import os
 
-image_name = Path("AI/test_data/0_image_4.png")
-
-model_path = Path("test.keras")
-
-
-
-def main_predict():
-    prediction, confidence = predict(image_name, model_path, None)
-
-    if prediction:
-        print(f"This is probably a {prediction}!\n"
-              f"I'm sure at {confidence * 100:.2f}%")
-
-def main_train():
-    train_model(5, model_path)
-
-def main_evaluate():
-    loss, accuracy = evaluate_model(model_path)
-    print(f"Model performance: Loss-> {loss}%, Accuracy-> {accuracy}%")
-
-def main_test():
-    results = test_model(model_path)
-
-    count = 0
-
-    for result in results:
-        image_nb, prediction, confidence, answer = result
-        print(  f"For image: {image_nb}\n"
-                f"Prediction: {prediction}\n"
-                f"Confidence: {confidence}\n"
-                f"Correct answer: {answer}\n")
-        if prediction == answer:
-            count += 1
-
-    print(f"Accuracy: {count/len(results)}")
+image_path = Path("AI/test_data")
+image_name = Path("0_image_0.png")
 
 
-main_predict()
+if __name__ == "__main__":
+    DigitModel = DigitRecognition("DigitRecognition.keras")
+
+    status = "continue"
+
+    while status == "continue":
+        os.system("clear")
+        print("Selected image: ", image_name, "\n",
+            "1- Select image\n",
+            "2- Train model\n",
+            "3- Evaluate model\n",
+            "4- Test model\n",
+            "5- Predict\n"
+        )
+        break
